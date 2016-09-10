7764d0c488d95e5c0393385122ddef045471c22a
stop2stare <emitremmus0235@163.com>
2016-09-10 23:49:55 +0800

    后台完善对前台请求跨域的支持以及github请求的逻辑

--- server/app.js
+++ server/app.js
@@ -2,12 +2,27 @@ var app = require('express')();
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var superagent = require('superagent');
+var cors = require('cors');
 
 var port = 4000;
+var host = 'https://api.github.com';
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('combined'));
 
-// app.get('');
+app.get('/search/repositories', function(req, res) {
+	var query = req.query.q;
+	superagent
+		.get(host + '/search/repositories')
+		.query('q=' + query)
+		.set('Accept', 'application/json')
+		.end(function(err, data) {
+			console.log(data.body)
+			res.json(data.body);
+		});
+});
 
 
 app.listen(port, function() {
