4f416dca3e1eb70394916412e462a072bb553861
刘成 <emitremmus0235@163.com>
2016-09-11 12:49:40 +0800

    添加登录逻辑，并且为以后的每个操作增加Authorization头信息

--- server/app.js
+++ server/app.js
@@ -12,18 +12,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('combined'));
 
+// 查找仓库，目前只支持了一个参数
 app.get('/search/repositories', function(req, res) {
 	var query = req.query.q;
+	var token = req.query.token;
+	if (!token) {
+		return res.json({status: false, error_message: '为了确保您的查询不被中断，请先登录！'});
+	}
 	superagent
 		.get(host + '/search/repositories')
 		.query('q=' + query)
 		.set('Accept', 'application/json')
+		.set('Authorization', 'token ' + token)
 		.end(function(err, data) {
 			console.log(data.body)
 			res.json(data.body);
 		});
 });
 
+// 登录
+app.get('/user', function(req, res) {
+	var access_token = req.query.access_token;
+	superagent
+		.get(host + '/user')
+		.set('Accept', 'application/json')
+		.end(function(err, data) {
+			if (err) {
+				return res.json({status: false, error_message: '请求错误'});
+			}
+
+			console.log(data);
+			res.json(data.body);
+		})
+})
 
 app.listen(port, function() {
 	console.log('server is running at http://locahost:%d', port);
