12bd761c4a158171e05e204f9cd156fe5936ca1e
stop2stare <emitremmus0235@163.com>
2016-09-12 23:27:28 +0800

    欢迎页根据登录情况展示不同文案

--- src/components/welcome/index.vue
+++ src/components/welcome/index.vue
@@ -1,10 +1,17 @@
 <script>
-	export default {}
+	import { getAvatarUrl } from '../../vuex/getters';
+	export default {
+		vuex: {
+			getters: {
+				avatar_url: getAvatarUrl
+			}
+		}
+	}
 </script>
 
 <template>
 	<div class="container">
-		<div class="welcome">github对于API请求有频率限制，请先登录，以获得更大的使用频率^_^</div>
+		<div class="welcome">{{ avatar_url ? '欢迎使用' : 'github对于API请求有频率限制，请先登录，以获得更大的使用频率^_^' }}</div>
 	</div>
 </template>
 
